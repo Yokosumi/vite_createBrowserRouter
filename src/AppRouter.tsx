7fbad21825d582bc785a9504796156fff9bad649
Selman Ciftci <selman.ciftci.coding@gmail.com>
2023-11-23 11:58:22 +0100

    nouns loader in

--- src/AppRouter.tsx
+++ src/AppRouter.tsx
@@ -1,6 +1,7 @@
 import App from "./App.tsx";
 import { createBrowserRouter, Navigate } from "react-router-dom";
 import { PageWelcome } from "./pages/PageWelcome.tsx";
+import { loader as nounLoader } from "./components/NounLoader.tsx";
 import { PageAbout } from "./pages/PageAbout.tsx";
 import { Page404 } from "./pages/Page404.tsx";
 import { PageTodos } from "./pages/PageTodos.tsx";
@@ -40,6 +41,7 @@ export const router = createBrowserRouter([
       {
         path: "/nouns",
         element: <PageNouns />,
+        loader: nounLoader,
       },
       {
         path: "/",
--- src/pages/PageNouns.tsx
+++ src/pages/PageNouns.tsx
@@ -1,6 +1,11 @@
+import { useLoaderData } from "react-router-dom";
+import { INoun } from "../interfaces";
+
 export const PageNouns = () => {
+  const nouns = useLoaderData() as INoun[];
   return (
     <>
+      {nouns.length}
       <div>nouns page</div>
     </>
   );
