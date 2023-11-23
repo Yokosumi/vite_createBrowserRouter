d02de074ca3227c3e8639b0344d7d2dad403192d
Selman Ciftci <selman.ciftci.coding@gmail.com>
2023-11-23 10:38:45 +0100

    Nav styles in

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ function App() {
   return (
     <>
       <h1 className="text-2xl mb-4">Data-Loading Site </h1>
+      <Nav />
       <Outlet />
     </>
   );
--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -4,7 +4,7 @@ export const Nav = () => {
   return (
     <>
       <nav>
-        <ul className="flex gap-3 bg-red-100 py-3 px-5 rounded">
+        <ul className="flex gap-3 bg-violet-700 py-3 px-5 my-3 rounded">
           <li>
             <NavLink to="/welcome"> Welcome</NavLink>
           </li>
