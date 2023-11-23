929025baaa36109763e5093e47ff6e57759872fa
Selman Ciftci <selman.ciftci.coding@gmail.com>
2023-11-23 15:58:51 +0100

    employees query works

--- src/pages/PageEmployees2.tsx
+++ src/pages/PageEmployees2.tsx
@@ -1,7 +1,26 @@
+import { useQuery } from "@tanstack/react-query";
+import { ImSpinner9 } from "react-icons/im";
+import { IEmployee } from "../interfaces";
+import * as appModel from "../appModel";
+
 export const PageEmployees2 = () => {
+  const employeesQuery = useQuery<IEmployee[]>({
+    queryKey: ["employees"],
+    queryFn: async () => appModel.getEmployees(),
+  });
+
+  if (employeesQuery.isLoading) {
+    return <ImSpinner9 className="animate-spin text-pink-500 text-7xl" />;
+  }
+  if (employeesQuery.isError) {
+    return <pre>there was an error</pre>;
+  }
+
   return (
     <>
-      <p>emp2 page</p>
+      {employeesQuery.data && (
+        <p> there are {employeesQuery.data?.length} employees</p>
+      )}
     </>
   );
 };
