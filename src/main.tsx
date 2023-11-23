814ad9c89da1b3ff7c30bd399f94b895bbca51b7
Selman Ciftci <selman.ciftci.coding@gmail.com>
2023-11-23 15:45:32 +0100

    QueryClient + QueryClientProvider in

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,12 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import { router } from "./AppRouter.tsx";
 import { RouterProvider } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <RouterProvider router={router} />
+  <QueryClientProvider client={queryClient}>
+    <RouterProvider router={router} />
+  </QueryClientProvider>
 );
