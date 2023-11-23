60837d29f6c130f0a86598afd837560acc518b43
Selman Ciftci <selman.ciftci.coding@gmail.com>
2023-11-23 15:05:41 +0100

    Spinner in

--- src/pages/PageEmployees1.tsx
+++ src/pages/PageEmployees1.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { IEmployee } from "../interfaces";
+import { ImSpinner9 } from "react-icons/im";
 import axios from "axios";
 
 export const PageEmployees1 = () => {
@@ -17,14 +18,22 @@ export const PageEmployees1 = () => {
 
   return (
     <>
-      <p className="text-4xl">There are {employees.length} employees:</p>
-      <ul>
-        {employees.map((employee) => (
-          <li className="text-3xl my-2" key={employee.employeeID}>
-            {employee.firstName} {employee.lastName}
-          </li>
-        ))}
-      </ul>
+      {employees.length !== 0 ? (
+        <>
+          <p className="text-4xl">There are {employees.length} employees:</p>
+          <ul>
+            {employees.map((employee) => (
+              <li className="text-3xl my-2" key={employee.employeeID}>
+                {employee.firstName} {employee.lastName}
+              </li>
+            ))}
+          </ul>
+        </>
+      ) : (
+        <div className="flex justify-center items-start h-screen mt-6">
+          <ImSpinner9 className="animate-spin text-pink-500 text-7xl" />
+        </div>
+      )}
     </>
   );
 };
