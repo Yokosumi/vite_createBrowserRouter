2703dc4c447f88de058d14696e00ae6771605f8f
Selman Ciftci <selman.ciftci.coding@gmail.com>
2023-11-23 14:01:03 +0100

    employees fetched and rendered

--- src/AppRouter.tsx
+++ src/AppRouter.tsx
@@ -1,7 +1,7 @@
 import App from "./App.tsx";
 import { createBrowserRouter, Navigate } from "react-router-dom";
 import { PageWelcome } from "./pages/PageWelcome.tsx";
-import { loader as nounLoader } from "./components/NounLoader.tsx";
+import { loader as nounLoader } from "./components/loader.tsx";
 import { PageAbout } from "./pages/PageAbout.tsx";
 import { Page404 } from "./pages/Page404.tsx";
 import { PageTodos } from "./pages/PageTodos.tsx";
--- src/pages/PageEmployees1.tsx
+++ src/pages/PageEmployees1.tsx
@@ -1,11 +1,30 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { IEmployee } from "../interfaces";
+import axios from "axios";
 
 export const PageEmployees1 = () => {
   const [employees, setEmployees] = useState<IEmployee[]>([]);
+
+  useEffect(() => {
+    (async () => {
+      const response = await axios.get(
+        "https://edwardtanguay.vercel.app/share/employees.json"
+      );
+      const _employees = response.data;
+      setEmployees(_employees);
+    })();
+  }, []);
+
   return (
     <>
       <p>There are {employees.length} employees:</p>
+      <ul>
+        {employees.map((employee) => (
+          <li className="text-3xl my-2" key={employee.employeeID}>
+            {employee.firstName} {employee.lastName} - {employee.title}
+          </li>
+        ))}
+      </ul>
     </>
   );
 };
